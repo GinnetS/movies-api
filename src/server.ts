2c41bb1ed6262d8cf71fcc47b49a11de41ab9fca
Ginnet Stephanie <103612210+GinnetS@users.noreply.github.com>
2025-09-27 11:36:54 -0500

    update procefile

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import * as dotenv from 'dotenv';
 dotenv.config();
+
 import { app } from './app';
 import { AppDataSource } from './shared/data-source';
 import { bootstrap } from './shared/bootstrap';
@@ -8,13 +9,25 @@ import { bootstrap } from './shared/bootstrap';
 const PORT = Number(process.env.PORT || 3000);
 
 async function main() {
+  const isProd = process.env.NODE_ENV === 'production';
   try {
-    await AppDataSource.initialize();
-    await bootstrap(); 
-    app.listen(PORT, '0.0.0.0', () => {
-    console.log(`[movies-api] listening on http://0.0.0.0:${PORT}`);
-});
+    if (isProd) {
+      if (process.env.DATABASE_URL) {
+        await AppDataSource.initialize();
+        console.log('DB initialized with DATABASE_URL');
+        await bootstrap();
+      } else {
+        console.log('No DATABASE_URL on Heroku. Skipping DB initialization for now.');
+      }
+    } else {
+ 
+      await AppDataSource.initialize();
+      await bootstrap();
+    }
 
+    app.listen(PORT, '0.0.0.0', () => {
+      console.log(`[movies-api] listening on http://0.0.0.0:${PORT}`);
+    });
   } catch (err) {
     console.error('Error initializing app:', err);
     process.exit(1);
